8a1e4d7761d58d7e0cf9bab56d93d6d19b883630
David Blackthorne <dblackthorne92@gmail.com>
2026-05-04 15:58:48 -0700

    added new code wars

---
+++ opposite-number.js
@@ -0,0 +1,13 @@
+// Challenge: given a number, find its opposite (additive inverse). 
+// Parameters: number will always be an integer. No other value types.
+// Return as a simple function
+// Ex. -1 = 1, 15 = -15, 30 = -30
+// Pseudo code:
+    // take the integer, multiply it by -1
+
+
+// My solution:
+
+function opposite(number) {
+  return -(number);
+}
