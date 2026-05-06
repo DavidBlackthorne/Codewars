6f2de9a60f71abaf5034c4f4b881acd9b13121ac
David Blackthorne <dblackthorne92@gmail.com>
2026-05-05 18:55:55 -0700

    added new codewars

---
+++ string-repeat.js
@@ -0,0 +1,19 @@
+// Challenge: Write a function that accepts a non-negative integer n and a string s as parameters, 
+// and returns a string of s repeated exactly n times.
+
+// parameters: integer will always be positive. 
+
+// return: return as a concatenated string.
+
+// Example: function (4, 'hello') = 'hellohellohellohello'
+
+// pseudocode: use string repeat method for 'n' times.
+
+// My solution:
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+
+
