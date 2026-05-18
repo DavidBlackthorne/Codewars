713713aa4e49229d1a700300b25da8a62afd952d
David Blackthorne <dblackthorne92@gmail.com>
2026-05-17 18:51:42 -0700

    added new code wars

---
+++ remove-string-spaces.js
@@ -0,0 +1,12 @@
+// Task: Write a function that removes the spaces from the string, then return the resultant string.
+
+// Parameters: the string will always be single string.
+// return as new string
+// example: "asdf jkl;" = "asdfjkl;"
+// pseudo code: use a string method to remove all empty spaces.
+
+// My solution:
+
+function noSpace(x){
+  return (x.replaceAll(' ', ''));
+}
